f48bd401554afb22738bd9f2db8ed2083ce09caa
Jaimekhc <jaimekhc@gmail.com>
2013-09-02 05:24:50 +0800

    fixed radio-pill selection at bindTimescopeDataToCurrentView()

--- app/assets/javascripts/views/timeframe_modal_view.js
+++ app/assets/javascripts/views/timeframe_modal_view.js
@@ -68,11 +68,11 @@ function bindTimescopeDataToCurrentView(timescope) {
     if(period !== "all") {
       var datefrom  = period.split(':')[0].split('/');
       var dateto    = period.split(':')[1].split('/');
-      $('input[name="timeframeRangeRadio"][value="range"]').attr('checked', true);
+      $('input[name="timeframeRangeRadio"][value="range"]').click();
       $('#timeframe-range-from').datepicker('setDate', new Date(datefrom[2], datefrom[0], datefrom[1]));
       $('#timeframe-range-to').datepicker('setDate', new Date(dateto[2], dateto[0], dateto[1]));
     } else {
-      $('input[name="timeframeRangeRadio"][value="all"]').attr('checked', true);
+      $('input[name="timeframeRangeRadio"][value="all"]').click();
     }
   }
 
@@ -87,13 +87,13 @@ function bindTimescopeDataToCurrentView(timescope) {
         $('#timeframe-container input[type="radio"][name="cycle"][value="year"]').click();
         if(Ember.isNone(calendar.weekdays)) {
           // 1st radio in block "year"
-          $('#timeframe-container input[type="radio"][name="cycle-year"][value="1"]').attr('checked', true);
+          $('#timeframe-container input[type="radio"][name="cycle-year"][value="1"]').click();
           $('#timeframe-cycle-year-select-1').val(calendar.months.selected_months[0]);
           $('#timeframe-cycle-year-select-2').val(calendar.months.number);
           $('#timeframe-cycle-year-repeat-number-1').val(repeat.number);
         } else {
           // 2nd radio in block "year"
-          $('#timeframe-container input[type="radio"][name="cycle-year"][value="2"]').attr('checked', true);
+          $('#timeframe-container input[type="radio"][name="cycle-year"][value="2"]').click();
           $('#timeframe-cycle-year-select-3').val(calendar.weekdays.number);
           $('#timeframe-cycle-year-select-4').val(calendar.weekdays.selected_days[0]);
           $('#timeframe-cycle-year-select-5').val(calendar.months.selected_months[0]);
@@ -105,12 +105,12 @@ function bindTimescopeDataToCurrentView(timescope) {
         $('#timeframe-container input[type="radio"][name="cycle"][value="month"]').click();
         if(!Ember.isNone(calendar.days)) {
           // 1st radio in block "month"
-          $('#timeframe-container input[type="radio"][name="cycle-month"][value="1"]').attr('checked', true);
+          $('#timeframe-container input[type="radio"][name="cycle-month"][value="1"]').click();
           $('#timeframe-cycle-month-select-1').val(calendar.days.number);
           $('#timeframe-cycle-month-repeat-number-1').val(repeat.number);
         } else {
           // 2nd radio in block "month"
-          $('#timeframe-container input[type="radio"][name="cycle-month"][value="2"]').attr('checked', true);
+          $('#timeframe-container input[type="radio"][name="cycle-month"][value="2"]').click();
           $('#timeframe-cycle-month-select-2').val(calendar.weekdays.number);
           $('#timeframe-cycle-month-select-3').val(calendar.weekdays.selected_days[0]);
           $('#timeframe-cycle-month-repeat-number-2').val(repeat.number);
